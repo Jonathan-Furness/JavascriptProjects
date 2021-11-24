02b68809779cc15c2d662181af4ea0e6f4999318
Jonathan Furness <jlsfurness@gmail.com>
2021-11-24 21:47:18 +0000

    Added a function to set a new quote

--- quote-generater/script.js
+++ quote-generater/script.js
@@ -1,11 +1,25 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+}
 
 // Get Quotes From API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
-    const apiQuotes = await response.json();
+    apiQuotes = await response.json();
+    newQuote();
   } catch (e) {
     // catch error
   }
